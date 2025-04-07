31150240429648a0e022951d2c43c2e079e81cad
Pinoso31 <jdespinozar@uamv.edu.ni>
2025-04-07 12:37:28 -0600

    semana 4

---
+++ operador_aritmetico.py
@@ -0,0 +1,6 @@
+#suma de dos numeros
+numero1 = int(input("Dime un numero : "))
+numero2 = int(input("Dime otro numero : "))
+suma = numero1 + numero2
+print(f"La suma de \n  {numero1:>9,} \n+ {numero2:>9,}\n------------ = {suma:>9,}")
+#\n para darle salto de linea
---
+++ operador_relacional.py
@@ -0,0 +1,3 @@
+num1 = 28
+num2 = 28
+print(not(num1))
