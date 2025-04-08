b23d8b5ed26125bb82a4dd7a0edd641e449d2dac
Pinoso31 <jdespinozar@uamv.edu.ni>
2025-04-07 19:54:45 -0600

    Semana 4

---
+++ datos_persona.py
@@ -0,0 +1,7 @@
+#asigna variables
+nombre = "Josue"
+edad = "20"
+estatura = "1.70"
+print("-"*60)
+print(f" Tu nombre es : {nombre} \n Tu edad es: {edad} años \n Eres de {estatura}m de alto" )
+print("-"*60)
---
+++ variables.py
@@ -0,0 +1,6 @@
+#Declara variables numericas
+a = 500
+b = 20
+c = 5
+resultado = (a + b) * c / 2
+print(f"El resultado es :  ", resultado)
