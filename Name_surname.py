afc17b7dc5de71d63ecaa2f5b9c4dceeabcf813a
Josue David Espinoza Ramirez <jdespinozar@uamv.edu.ni>
2025-04-08 15:46:33 -0600

    Name_surnamee.py

--- Name_surname.py
+++ Name_surname.py
@@ -1,4 +1,4 @@
 #haz un algoritmo que reciba el nombre y apellido de una persona y lo contatenes
 nombres = "Josue David "
 apellidos = "Espinoza Ramirez"
-print("Tu nombre completo es : {nombres} {apellidos}")
+print(f"Tu nombre completo es : {nombres} {apellidos}")
